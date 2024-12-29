caf40e6b0a719ac47477322155e55e3da57ce29b
Rhafael Chandra <rhafaelchandra@gmail.com>
2024-12-29 21:09:17 +0700

    lsp keymaps

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -2,13 +2,27 @@
   pkgs,
   ...
 }: {
-  plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        ts_ls.enable = true;
-        lua_ls.enable = true;
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      ts_ls.enable = true;
+      lua_ls.enable = true;
+    };
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
       };
+
+      lspBuf = {
+          K = "hover";
+          gD = "references";
+          gd = "definition";
+          gi = "implementation";
+          gt = "type_definition";
+      };
+
+
     };
   };
 }
