551b6ec07ab01277472201fa47b753bb1328f0e7
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-15 08:31:24 +0700

    fix: dap ui wont show up

--- config/languages/debugging.nix
+++ config/languages/debugging.nix
@@ -1,8 +1,21 @@
 {
   plugins = {
-    dap.enable = true;
+    dap = {
+      enable = true;
+      extensions = {
+        dap-ui = {
+          enable = true;
+          floating.mappings = {close = ["<ESC>" "q"];};
+        };
+        dap-virtual-text = {enable = true;};
+      };
+    };
   };
-
+  extraConfigLua = ''
+    require('dap').listeners.after.event_initialized['dapui_config'] = require('dapui').open
+    require('dap').listeners.before.event_terminated['dapui_config'] = require('dapui').close
+    require('dap').listeners.before.event_exited['dapui_config'] = require('dapui').close
+  '';
   keymaps = [
     {
       key = "<leader>db";
--- config/languages/golang/debugging.nix
+++ config/languages/golang/debugging.nix
@@ -1,6 +1,5 @@
-{ pkgs, ... }: {
+{
   plugins.dap.extensions.dap-go = {
     enable = true;
-    delve.path = "${pkgs.delve}/bin/dlv";
   };
 }
