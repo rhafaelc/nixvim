8f0f3ebfbc40f0ef78cb06be08d1b64ab5a008d4
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-15 10:13:23 +0700

    feat: add go.nvim

--- config/languages/golang/language.nix
+++ config/languages/golang/language.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   plugins = {
     conform-nvim.settings.formatters_by_ft = {
       go = ["golines"];
@@ -16,4 +16,22 @@
     };
     none-ls.sources.formatting.golines.enable = true;
   };
+  extraPlugins = [pkgs.vimPlugins."go-nvim"];
+  extraConfigLua = "require('go').setup()";
+  keymaps = [
+    {
+      key = "<leader>gsj";
+      action = ":GoAddTag json<cr>";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      key = "<leader>gsy";
+      action = ":GoAddTag yaml<cr>";
+      options = {
+        silent = true;
+      };
+    }
+  ];
 }
